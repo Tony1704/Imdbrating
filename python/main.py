4427cdcc7b2b4d938cdb53c9389029418e70365f
Tony1704 <anton.ochel1@gmail.com>
2021-03-17 16:18:20 +0100

    created ml model

--- python/main.py
+++ python/main.py
@@ -6,6 +6,7 @@ from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 
+
 def mltesting(movies):
     nn = MLPRegressor(hidden_layer_sizes=100, activation="logistic", solver="adam", verbose=True, max_iter=3000)
     x = []
@@ -29,22 +30,77 @@ def mltesting(movies):
     print(nn.score(x_test.astype(np.float64), y_test))
     # print(nn.predict([[120, 2005, 60000]]))
 
+def learn(nnmovies,ratings):
+    nn = MLPRegressor(hidden_layer_sizes=200, activation="logistic", solver="adam", verbose=True, max_iter=3000, learning_rate="adaptive")
+    x = []
+    y = []
+    for line in nnmovies:
+        x.append(line)
+    for rating in ratings:
+        y.append(int(rating))
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.5)
+    print(x_test,y_test)
+    nn.fit(x_train, y_train)
+    print(nn.out_activation_)
+    print(nn.score(x_test, y_test))
+    return nn
 
 def testing(movies):
     for line in movies:
         print(line.getAsList())
 
-def labelBinariser(movies):
+
+def personIdEncoder(movies):
     db = database_connector.DataBase()
     nconsts = db.get_all_person_id()
-    print(nconsts)
-    lb = preprocessing.LabelBinarizer()
-    print(lb.fit_transform(nconsts))
-    print(lb.transform(['nm0051659, nm2860379']))
+    lb = preprocessing.LabelEncoder()
+    lb.fit(nconsts)
+    return lb
+
 
 def printallmovies(movies):
     for movie in movies:
         print(movie.getAsList())
+
+
+def getAllGenres(movies):
+    genres = []
+    for movie in movies:
+        for genre in movie.genres:
+            if genre in genres:
+                continue
+            else:
+                genres.append(genre)
+    return genres
+
+def getYValues(movies):
+    y = []
+    for movie in movies:
+        y.append(movie.averageRating)
+    return y
+
+def getAllRoles(movies):
+    roles = []
+    for movie in movies:
+        for actor in movie.actors:
+            if actor.category in roles:
+                continue
+            else:
+                roles.append(actor.category)
+    return roles
+
+
+def binariseRoles(roles):
+    lb = preprocessing.LabelBinarizer()
+    lb.fit(roles)
+    return lb
+
+
+def binariseGenres():
+    genres = getAllGenres(loadDataBase())
+    lb = preprocessing.LabelBinarizer()
+    lb.fit(genres)
+    return lb
 
 
 def loadDataBase():
@@ -58,5 +114,55 @@ def loadDataBase():
     return movies
 
 
+def prepareMoviesForNN(movies):
+    mlmovies = []
+    genresBinarizer = binariseGenres()
+    roleBinarizer = binariseRoles(getAllRoles(movies))
+    personEncoder = personIdEncoder(movies)
+    for movie in movies:
+        genre1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        genre2 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        genre3 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        if movie.genres is not None:
+            genre1 = np.array(genresBinarizer.transform([movie.genres[0]])[0])
+            if len(movie.genres) >= 2:
+                genre2 = np.array(genresBinarizer.transform([movie.genres[1]])[0])
+            if len(movie.genres) >= 3:
+                genre3 = np.array(genresBinarizer.transform([movie.genres[2]])[0])
+        genre =  np.add(genre1,genre2,genre3)
+        actors = []
+        #for i in range(10):
+        #    try:
+        #        personId = personEncoder.transform([movie.actors[i].nconst])[0]
+        #        #array = np.array([personId])
+        #        ordering = movie.actors[i].ordering
+        #        roleArray = roleBinarizer.transform([movie.actors[i].category])[0]
+        #        array = np.array([personId, ordering])
+        #        array = np.concatenate((array, roleArray))
+        #        actors.append(array)
+        #    except IndexError:
+        #        array = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        #        #array = np.array([0])
+        #        actors.append(array)
+        mlmovie = np.array([movie.startYear,movie.runtimeMinutes])
+        mlmovie = np.concatenate((mlmovie,genre))
+        for actor in actors:
+            mlmovie = np.concatenate((mlmovie,actor))
+        mlmovies.append(mlmovie)
+    return mlmovies
+
+def predictMovie(startYear, runtime, genre1, genre2, genre3):
+    genresBinarizer = binariseGenres()
+    genre1 = np.array(genresBinarizer.transform([genre1])[0])
+    genre2 = np.array(genresBinarizer.transform([genre2])[0])
+    genre3= np.array(genresBinarizer.transform([genre3])[0])
+    genre = np.add(genre1, genre2, genre3)
+    mlmovie = np.array([startYear, runtime])
+    mlmovie = np.concatenate((mlmovie,genre))
+    mlmovie = mlmovie.reshape(1, -1)
+    return mlmovie
+
+
 if __name__ == '__main__':
-    printallmovies(loadDataBase())
+    moviePredictor = learn(prepareMoviesForNN(loadDataBase()),getYValues(loadDataBase()))
+    print(moviePredictor.predict(predictMovie(2002,90,"Romance", "Comedy","Thriller")))
