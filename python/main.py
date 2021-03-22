f92b73225168ed5f2ecfd12d01ecba8f2521ab50
Tony1704 <anton.ochel1@gmail.com>
2021-03-22 16:16:57 +0100

    worked on ml

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 import database_connector
 import movie
 import numpy as np
-from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPRegressor, MLPClassifier
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
@@ -30,27 +30,29 @@ def mltesting(movies):
     print(nn.score(x_test.astype(np.float64), y_test))
     # print(nn.predict([[120, 2005, 60000]]))
 
-def learn(nnmovies,ratings):
-    nn = MLPRegressor(hidden_layer_sizes=200, activation="logistic", solver="adam", verbose=True, max_iter=3000, learning_rate="adaptive")
+
+def learn(nnmovies, ratings):
+    nn = MLPClassifier(hidden_layer_sizes=200, activation="tanh", solver="sgd", verbose=True, max_iter=3000)
     x = []
     y = []
     for line in nnmovies:
         x.append(line)
     for rating in ratings:
         y.append(int(rating))
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.5)
-    print(x_test,y_test)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.3)
+    print(x_test, y_test)
     nn.fit(x_train, y_train)
     print(nn.out_activation_)
     print(nn.score(x_test, y_test))
     return nn
+
 
 def testing(movies):
     for line in movies:
         print(line.getAsList())
 
 
-def personIdEncoder(movies):
+def personIdEncoder():
     db = database_connector.DataBase()
     nconsts = db.get_all_person_id()
     lb = preprocessing.LabelEncoder()
@@ -73,13 +75,16 @@ def getAllGenres(movies):
                 genres.append(genre)
     return genres
 
+
 def getYValues(movies):
     y = []
     for movie in movies:
         y.append(movie.averageRating)
     return y
 
-def getAllRoles(movies):
+
+def getAllRoles():
+    movies = loadDataBase()
     roles = []
     for movie in movies:
         for actor in movie.actors:
@@ -90,7 +95,8 @@ def getAllRoles(movies):
     return roles
 
 
-def binariseRoles(roles):
+def binariseRoles():
+    roles= getAllRoles()
     lb = preprocessing.LabelBinarizer()
     lb.fit(roles)
     return lb
@@ -117,8 +123,8 @@ def loadDataBase():
 def prepareMoviesForNN(movies):
     mlmovies = []
     genresBinarizer = binariseGenres()
-    roleBinarizer = binariseRoles(getAllRoles(movies))
-    personEncoder = personIdEncoder(movies)
+    roleBinarizer = binariseRoles()
+    personEncoder = personIdEncoder()
     for movie in movies:
         genre1 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         genre2 = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
@@ -129,40 +135,51 @@ def prepareMoviesForNN(movies):
                 genre2 = np.array(genresBinarizer.transform([movie.genres[1]])[0])
             if len(movie.genres) >= 3:
                 genre3 = np.array(genresBinarizer.transform([movie.genres[2]])[0])
-        genre =  np.add(genre1,genre2,genre3)
+        genre = np.add(genre1, genre2)
+        genre = np.add(genre, genre3)
         actors = []
-        #for i in range(10):
-        #    try:
-        #        personId = personEncoder.transform([movie.actors[i].nconst])[0]
-        #        #array = np.array([personId])
-        #        ordering = movie.actors[i].ordering
-        #        roleArray = roleBinarizer.transform([movie.actors[i].category])[0]
-        #        array = np.array([personId, ordering])
-        #        array = np.concatenate((array, roleArray))
-        #        actors.append(array)
-        #    except IndexError:
-        #        array = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        #        #array = np.array([0])
-        #        actors.append(array)
-        mlmovie = np.array([movie.startYear,movie.runtimeMinutes])
-        mlmovie = np.concatenate((mlmovie,genre))
+        for i in range(10):
+            try:
+                personId = personEncoder.transform([movie.actors[i].nconst])[0]
+                #array = np.array([personId])
+                ordering = movie.actors[i].ordering
+                roleArray = roleBinarizer.transform([movie.actors[i].category])[0]
+                array = np.array([personId, ordering])
+                array = np.concatenate((array, roleArray))
+                actors.append(array)
+            except IndexError:
+                array = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+               #array = np.array([0])
+                actors.append(array)
+        mlmovie = np.array([movie.startYear, movie.runtimeMinutes])
+        mlmovie = np.concatenate((mlmovie, genre))
         for actor in actors:
-            mlmovie = np.concatenate((mlmovie,actor))
+            mlmovie = np.concatenate((mlmovie, actor))
         mlmovies.append(mlmovie)
     return mlmovies
 
-def predictMovie(startYear, runtime, genre1, genre2, genre3):
-    genresBinarizer = binariseGenres()
-    genre1 = np.array(genresBinarizer.transform([genre1])[0])
-    genre2 = np.array(genresBinarizer.transform([genre2])[0])
-    genre3= np.array(genresBinarizer.transform([genre3])[0])
-    genre = np.add(genre1, genre2, genre3)
-    mlmovie = np.array([startYear, runtime])
-    mlmovie = np.concatenate((mlmovie,genre))
-    mlmovie = mlmovie.reshape(1, -1)
-    return mlmovie
 
+def createMovie(startYear, runtime, genre1, genre2, genre3):
+    genres = "" + genre1
+    if genre2 != "":
+        genres = genres + "," + genre2
+    if genre3 != "":
+        genres = genres + "," + genre3
+    array = [0,"",startYear, runtime,genres,0,0]
+    newMovie = movie.Movie(array)
+    return newMovie
 
 if __name__ == '__main__':
-    moviePredictor = learn(prepareMoviesForNN(loadDataBase()),getYValues(loadDataBase()))
-    print(moviePredictor.predict(predictMovie(2002,90,"Romance", "Comedy","Thriller")))
+    moviePredictor = learn(prepareMoviesForNN(loadDataBase()), getYValues(loadDataBase()))
+    ourMovie = createMovie(1995,142,"Drama","","")
+    ourMovie.addCrewByName("Frank Darabont","director")
+    ourMovie.addCrewByName("Morgan Freeman", "actor")
+    ourMovie.addCrewByName("Tim Robbins", "actor")
+    print(ourMovie.genres)
+    ournnMovie = prepareMoviesForNN([ourMovie])
+    print(ournnMovie[0])
+    print(moviePredictor.predict(ournnMovie))
+
+
+
+
