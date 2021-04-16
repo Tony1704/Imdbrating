e6d1fd9f407a3fb6e160963f42ccce40aa12de12
Tony1704 <anton.ochel1@gmail.com>
2021-04-16 17:18:02 +0200

    Changes by alrichert

--- python/main.py
+++ python/main.py
@@ -40,7 +40,6 @@ def printallmovies(movies):
     for movie in movies:
         print(movie.getAsList())
 
-
 # Gibt zeit und prozent aus
 def _secondsToStr(t):
     return "%d:%02d:%02d.%03d" % reduce(lambda ll, b: divmod(ll[0], b) + ll[1:], [(t * 1000,), 1000, 60, 60])
